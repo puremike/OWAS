f1ea6e70505df51b794fe74312babe75bd15ebd1
puremike <michaelmarketing25@gmail.com>
2025-06-03 23:48:00 -0500

    starting the auction api

---
+++ online_auction_api/cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/basic/docs"
+)
+
+func (app *application) routes() http.Handler {
+
+	g := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
+	api := g.Group("/api/v1")
+	{
+		api.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	}
+
+	return g
+}
